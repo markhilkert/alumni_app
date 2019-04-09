f379944ba0ad068a533fb92e083bd93b35a5e7ff
farfalle211 <farfalle211@gmail.com>
2019-04-09 15:18:51 -0500

    added enums

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,3 @@
 class Project < ApplicationRecord
+  enum type: {capstone: 0, graduate_initiative: 1, personal: 2, in_class: 3}
 end
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,2 +1,3 @@
 class Resource < ApplicationRecord
+  enum category: {job_websites: 0, job_informational: 1, coding_practice: 2, cheat_sheets: 3}
 end
